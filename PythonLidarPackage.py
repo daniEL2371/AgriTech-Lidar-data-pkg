84929cf40e44acafc833316e7baa2da11e0345e4
Daniel <danielzelalemheru@gmail.com>
2021-08-20 01:26:50 +0300

    fix: cache filename error

--- PythonLidarPackage.py
+++ PythonLidarPackage.py
@@ -5,24 +5,25 @@ from elevation_extractor import ElevationExtractor
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import pickle
-
+import os
 
 
 PUBLIC_DATA_URL = "https://s3-us-west-2.amazonaws.com/usgs-lidar-public/"
 cache_folder = "./cache"
 
 
-
 def read_obj(file_name):
-        with open(f"{cache_folder}/{file_name}.pkl", "rb") as f:
-            return pickle.load(f)
+    with open(f"{cache_folder}/{file_name}.pkl", "rb") as f:
+        return pickle.load(f)
+
 
 def write_obj(file_name, obj):
     if (not os.path.isdir('./cache')):
         os.mkdir("./cache/")
-    
+
     with open(f"{cache_folder}/{file_name}.pkl", "wb") as f:
         pickle.dump(obj, f)
+
 
 def get_cache_name_from_polygon(polygon: Polygon):
     x, y = polygon.exterior.coords.xy
@@ -32,87 +33,82 @@ def get_cache_name_from_polygon(polygon: Polygon):
         temp += f"{i}_{j}"
 
     hashed_name = hash(temp)
-    
+
     if hashed_name < 0:
         hashed_name = "a" + str(hashed_name)[1:]
-    
+
     return hashed_name
-        
+
+
 def cache_fetched_data(file_name: str, obj):
 
     try:
-        write_model(str(file_name), obj)
+        write_obj(str(file_name), obj)
     except:
         pass
-        
+
 
 class PythonLidarPackage:
 
-    def __init__(self, crs_epsg : int) -> None:
-        
+    def __init__(self, crs_epsg: int) -> None:
+
         self.epsg = crs_epsg
         self.fetcher = Lidar_Data_Fetch(PUBLIC_DATA_URL, epsg=epsg)
         self.ee = ElevationExtractor(crs_epgs=crs_epsg)
-    
+
     def __get_regions(self, polygon: Polygon) -> list:
         pass
-    
-    def get_elevation_df(self, polygon: Polygon, from_cache=True, enforce_cache = True):
-        
+
+    def get_elevation_df(self, polygon: Polygon, from_cache=True, enforce_cache=True):
+
         result = dict()
-        
-        cache = get_cache_name_from_polygon(polygon)
-        
-        if (from_cache and os.path.isfile(cahce_file_name)):
-            return read_obj(cahce_file_name)
-            
-        
+
+        cache_file_name = get_cache_name_from_polygon(polygon)
+
+        if (from_cache and os.path.isfile(cache_file_name)):
+            return read_obj(cache_file_name)
+
         regions_year_tuple = self.__get_regions()
-        
+
         for region, year in regions_year_tuple:
-            
+
             if year == 0:
                 year = "Unknown"
-            
+
             data, output_epsg = self.fetcher.runPipeline(region, polygon)
             df = self.ee.get_elevetion(data)
             result[region] = (year, df)
-        
+
         if enforce_cache:
-            cache_fetched_data(file_name, result)
-        
+            cache_fetched_data(cache_file_name, result)
+
         return result
-    
+
     def save_elevation_geodata(self, df, polygon: Polygon,  file_name: str, save_format="shp"):
-        
+
         polygon_df = gpd.GeoDataFrame([polygon], columns=['geometry'])
         polygon_df.set_crs(epsg=self.epsg, inplace=True)
-        
+
         if save_format == "shp":
             df.to_file(f"{file_name}_elevation.shp")
             polygon_df.to_file(f"{file_name}_boundaries.shp")
-        
+
         elif save_format == "geojson":
-            countries_gdf.to_file(f"{file_name}_elevation.geojson", driver='GeoJSON')
-            polygon_df.to_file(f"{file_name}_boundaries.geojson", driver='GeoJSON')
-        
+            df.to_file(
+                f"{file_name}_elevation.geojson", driver='GeoJSON')
+            polygon_df.to_file(
+                f"{file_name}_boundaries.geojson", driver='GeoJSON')
+
         else:
             print("Unsupported format, geojson and shp are only supported formats")
 
-    
-    def get_heatmap_visulazation(self, df: gpd.GeoDataFrame, cmap="terrain") -> None :
+    def get_heatmap_visulazation(self, df: gpd.GeoDataFrame, cmap="terrain") -> None:
 
         fig, ax = plt.subplots(1, 1, figsize=(12, 10))
 
         df.plot(column='elevation', ax=ax, legend=True, cmap=cmap)
         plt.show()
-    
+
     def covert_crs(self, df: gpd.GeoDataFrame, crs_epgs: int) -> gpd.GeoDataFrame:
-        
+
         return self.ee.covert_crs(crs_epgs, df)
-
-
-
-            
-            
-
