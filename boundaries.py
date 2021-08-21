23a695f3b2319a3cb95f919634a453a9e3764d2b
Daniel <danielzelalemheru@gmail.com>
2021-08-21 20:32:35 +0300

    doc: add docstring for boundaries  module

--- boundaries.py
+++ boundaries.py
@@ -1,5 +1,16 @@
 class Boundaries:
+    """A class represntation of a rectangular bound
+    """
+
     def __init__(self, ymin: float, xmin: float, ymax: float, xmax: float) -> None:
+        """Boundaries class is instantiated by providing x minmum, y minmum,  x maximum, y maximum coordinates defining a rectangular bounds
+
+        Args:
+            ymin (float)
+            xmin (float)
+            ymax (float)
+            xmax (float)
+        """
         self.ymin = ymin
         self.xmin = xmin
         self.ymax = ymax
@@ -7,7 +18,11 @@ class Boundaries:
         pass
 
     def getBounds(self) -> tuple:
+        """Returns the extreme points defining the rectangular bounds in a list in the order of xmin, ymin, ymax, xmax
+        """
         return ([self.xmin, self.ymin], [self.ymax, self.xmax])
 
     def getBoundStr(self) -> str:
-        return f"([{self.xmin}, {self.ymin}], [{self.ymax}, {self.xmax}])"
+        """Return a string representation of the extreme points defining the rectangular bounds in a '({[minx, maxx]},{[miny,maxy]})' format (i.e the format our pdal pipeline's reader.ept (https://pdal.io/stages/readers.ept.html#readers-ept) expects
+        """
+        return f"({[self.xmin, self.xmax]},{[self.ymin, self.ymax]})"
